2377f43353955c649065817a076f189fb7f40f9c
rune <rune_hartvig@hotmail.com>
2021-04-20 11:59:02 +0200

    adds one more unit test for a-instruction translation

--- unit.test.py
+++ unit.test.py
@@ -1,6 +1,8 @@
 from assembler import translate_a_instruction
 
 assert translate_a_instruction('@1') == '000000000000001', "should be 000000000000001"
+assert translate_a_instruction('@9') == '000000000001001', "should be 000000000001001"
+
 
 print('All tests passed')
 
