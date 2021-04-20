b2ebba3e3206968ed3c9d28afe3c730c0f98a0e6
rune <rune_hartvig@hotmail.com>
2021-04-20 08:30:13 +0200

    simple read from .asm file

--- assembler.py
+++ assembler.py
@@ -1,5 +1,14 @@
-def main():
-    print("Hello world")
+import sys
+
+#if len(sys.argv) !=2:
+#    raise ValueError('Please provide the name of an assembler file as the only argument.')
+
+def main(filename):
+    with open(filename, "r") as asm_file:
+        data = asm_file.read()
+        print(data)
 
 if __name__ == "__main__":
-    main()
+    filename = sys.argv[1]
+
+    main(filename)
